05dbaf72339b1fef5a615b715d1f75c580316987
Lars Wallenborn <lars@wallenborn.net>
2015-05-18 08:18:28 +0200

    downloaded some more functions from the internet

---
+++ wlan/wlan-functions.ps1
@@ -0,0 +1,35 @@
+# source: http://www.leeholmes.com/blog/2013/06/06/removing-insecure-wireless-connections-with-powershell/
+# @todo: check if it also works on non-english windows
+
+function Get-WlanProfile
+{
+    netsh wlan show all |
+        sls "^[\s]+Name\s+:" -Context 16 |
+        % {
+            $conn = [Ordered] @{}
+            $_.Line, $_.Context.PostContext[1], $_.Context.PostContext[15] | % {
+                $label,$value = $_ -split ':'
+                $conn[$label.Trim()] = $value.Trim()
+            }
+            $result = [PSCustomObject] $conn
+            if($result.Name -and $result.Authentication) { $result }
+        }
+}
+
+function Get-UnsecureWlanProfile
+{
+    Get-WlanProfile | ? {
+        ($_.Authentication -eq 'Open') -and
+        ($_."Connection mode" -match "automatically")
+    }
+}
+
+function Remove-WlanProfile
+{
+    param(
+        [Parameter(ValueFromPipelineByPropertyName)]
+        $Name
+    )
+
+    netsh wlan delete profile name="$Name"
+}
